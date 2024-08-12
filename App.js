8948c4e9dfa961b0ac3e216b05b59379e4c892f2
usman <usman.hasan619@gmail.com>
2024-08-13 01:17:05 +0530

    fix

--- App.js
+++ App.js
@@ -6,34 +6,12 @@ import AuthRoute from "./src/Routes/Auth.route";
 import store from "./src/redux/stores/store";
 import Toast from "react-native-toast-message";
 import { PaperProvider } from "react-native-paper";
-import CollapsibleViewComponent from "./src/components/CollapsibleView.component";
 
 export default function App() {
-  const [headerScroll, setHeaderScroll] = React.useState(false);
-
-  const onScroll = (event) => {
-    console.log("event: ", event);
-    if (event?.nativeEvent?.contentOffset?.y > 50) {
-      setHeaderScroll(true);
-    } else {
-      setHeaderScroll(false);
-    }
-  };
   return (
     <Provider store={store}>
       <PaperProvider>
         <NavigationContainer independent={true}>
-          {/* <CollapsibleViewComponent
-            className="fixed top-0 left-0 right-0 "
-            style={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-              right: 0,
-              zIndex: 1,
-              width: "100%",
-            }}
-          /> */}
           <AuthRoute />
         </NavigationContainer>
         <Toast topOffset={55} style={{ elevation: 99, zIndex: 99 }} />
